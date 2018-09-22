09114a237a0d7f28cb30a022dc50e1e8e176f6cd
linchengzzz <1139489076@qq.com>
2018-09-22 20:18:14 +0800

    ang

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
+import { NodeLogger } from '@angular/core/src/view';
 @Injectable({
     providedIn: 'root',
 })
 export class HeroService {
-    constructor() {}
+    constructor(
+        private logger: NodeLogger,
+        private isAuthorized: boolean) {}
     getHeroes(): Hero[] {
+        let auth = this.isAuthorized ? 'authorized' : 'unauthorized';
         return HEROES;
     }
 }
